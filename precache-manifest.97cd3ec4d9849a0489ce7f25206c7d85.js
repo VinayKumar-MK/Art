427e22259e661d92c5b575613ea45b050e8ecfd4
VinayKumar-MK <datasourcethbs@gmail.com>
2021-12-05 16:32:20 +0530

    Updates

---
+++ precache-manifest.97cd3ec4d9849a0489ce7f25206c7d85.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2a6dbad5d74546b2e3e94b9a19e77d",
+    "url": "/Art/index.html"
+  },
+  {
+    "revision": "ad98a9b4f014b00e5982",
+    "url": "/Art/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "0a14628dbf1dd150a7c0",
+    "url": "/Art/static/css/main.862f5c49.chunk.css"
+  },
+  {
+    "revision": "ad98a9b4f014b00e5982",
+    "url": "/Art/static/js/2.333c7804.chunk.js"
+  },
+  {
+    "revision": "0a14628dbf1dd150a7c0",
+    "url": "/Art/static/js/main.0c82eae4.chunk.js"
+  },
+  {
+    "revision": "75f9c6eece7e4a430991",
+    "url": "/Art/static/js/runtime~main.f4f0bb35.js"
+  },
+  {
+    "revision": "d1e93e9d52086336140e7c9a3a2fd121",
+    "url": "/Art/static/media/a.d1e93e9d.jpg"
+  },
+  {
+    "revision": "6acfa53a32c9733a258bc753870fbd17",
+    "url": "/Art/static/media/aa.6acfa53a.jpg"
+  },
+  {
+    "revision": "98c42ecb88764f215e1eceae47c1bbdd",
+    "url": "/Art/static/media/bb.98c42ecb.jpg"
+  },
+  {
+    "revision": "6e68885a940a60dd4886366e43032ac3",
+    "url": "/Art/static/media/c.6e68885a.jpg"
+  },
+  {
+    "revision": "6f44388aef3fef03dc0a65afc1823688",
+    "url": "/Art/static/media/c1.6f44388a.jpg"
+  },
+  {
+    "revision": "69d9107a0de793344763efad47dcc490",
+    "url": "/Art/static/media/c2.69d9107a.jpg"
+  },
+  {
+    "revision": "868f879b681f3c008c55bf3a79d366e9",
+    "url": "/Art/static/media/c3.868f879b.jpg"
+  },
+  {
+    "revision": "2aa91daf3feb1e8376988dfa05c9c52b",
+    "url": "/Art/static/media/cc.2aa91daf.jpg"
+  },
+  {
+    "revision": "db16fc87f202d7bcc5b285212bf082f1",
+    "url": "/Art/static/media/d.db16fc87.jpg"
+  },
+  {
+    "revision": "c81309f671913d5a17df1a1b294ab4b3",
+    "url": "/Art/static/media/dd.c81309f6.jpg"
+  },
+  {
+    "revision": "3e729afcff9737da98fc5133c9048a0a",
+    "url": "/Art/static/media/ee.3e729afc.jpg"
+  },
+  {
+    "revision": "2742a77db5b77a08dc02c67885fd10bb",
+    "url": "/Art/static/media/f.2742a77d.jpg"
+  },
+  {
+    "revision": "43b25ab9f3efa38981f63e88f7c41785",
+    "url": "/Art/static/media/ff.43b25ab9.jpg"
+  },
+  {
+    "revision": "841040f87eab0e6a283e85703a0ded39",
+    "url": "/Art/static/media/gg.841040f8.jpg"
+  },
+  {
+    "revision": "d5cc0a79c069984657b2a463d76c1d1d",
+    "url": "/Art/static/media/h.d5cc0a79.jpg"
+  },
+  {
+    "revision": "d9cb318aca4128fb95dd37007b4dc091",
+    "url": "/Art/static/media/hh.d9cb318a.jpg"
+  },
+  {
+    "revision": "7045fac04599989572edb544cac04ca2",
+    "url": "/Art/static/media/i.7045fac0.jpg"
+  },
+  {
+    "revision": "026e65816777a656fc323ebe1bfdbee9",
+    "url": "/Art/static/media/ii.026e6581.jpg"
+  },
+  {
+    "revision": "497eac32a96e79ba15d8a795d237b198",
+    "url": "/Art/static/media/j.497eac32.jpg"
+  },
+  {
+    "revision": "d568e768595f3246878a8d9537ab761c",
+    "url": "/Art/static/media/jj.d568e768.jpg"
+  },
+  {
+    "revision": "3dd6eec372bd78bea1aa82df79cdf858",
+    "url": "/Art/static/media/kk.3dd6eec3.jpg"
+  },
+  {
+    "revision": "e67211cf4e41eed6764706c4c912ca3d",
+    "url": "/Art/static/media/l.e67211cf.jpg"
+  },
+  {
+    "revision": "a2e404641caa19d162494833a35acf1a",
+    "url": "/Art/static/media/ll.a2e40464.jpg"
+  },
+  {
+    "revision": "042c8b84cd307615b6cd38802d454824",
+    "url": "/Art/static/media/m.042c8b84.jpg"
+  },
+  {
+    "revision": "780fc8e21132851edefc1f32c9ee639c",
+    "url": "/Art/static/media/mm.780fc8e2.jpg"
+  },
+  {
+    "revision": "317b1c1012e3eeb09215b32bae20e910",
+    "url": "/Art/static/media/nn.317b1c10.jpg"
+  },
+  {
+    "revision": "c5d43055dceb3c5fcadca986d68c5476",
+    "url": "/Art/static/media/o.c5d43055.jpg"
+  },
+  {
+    "revision": "1d5faa931979f02da62fbe9d8328b11b",
+    "url": "/Art/static/media/oo.1d5faa93.jpg"
+  },
+  {
+    "revision": "f37adf5a45908dd159e7ba132f756988",
+    "url": "/Art/static/media/p.f37adf5a.jpg"
+  },
+  {
+    "revision": "bf876cd0488444c346df03d4171cf6d5",
+    "url": "/Art/static/media/pp.bf876cd0.jpg"
+  },
+  {
+    "revision": "5b0f4a19ecdfbfbdbebd688fb49e82cb",
+    "url": "/Art/static/media/q.5b0f4a19.jpg"
+  },
+  {
+    "revision": "36f2508579528966eb5917d86aaaafae",
+    "url": "/Art/static/media/r.36f25085.jpg"
+  },
+  {
+    "revision": "3cf4fc13e037fad1d587cce3cc9af8ed",
+    "url": "/Art/static/media/s.3cf4fc13.jpg"
+  },
+  {
+    "revision": "d00c514a29896b7f3371c5e9d1eab252",
+    "url": "/Art/static/media/t.d00c514a.jpg"
+  },
+  {
+    "revision": "415dddec72c24c62d84a26b307e5e071",
+    "url": "/Art/static/media/u.415dddec.jpg"
+  },
+  {
+    "revision": "29c5a0c295de03430eb8394faddfab5c",
+    "url": "/Art/static/media/v.29c5a0c2.jpg"
+  }
+]);
